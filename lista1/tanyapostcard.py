37825bde3f4cc1166109cdf65364208d66caccd1
adrianmartinez-cg <pedro.martinez@ccc.ufcg.edu.br>
2023-07-31 07:02:08 -0300

    lista1: tanya and postcard [incomplete2]

--- lista1/tanyapostcard.py
+++ lista1/tanyapostcard.py
@@ -1,33 +1,41 @@
-def checkLetters(str,letters):
+def checkMatches(s,t,caseL,L):
+    y = 0
+    w = 0
+    m = len(s)
     left = 0
-    right = len(str) - 1
+    right = m - 1
     while left <= right:
-        if str[left] in letters:
-            letters[str[left]] += 1
+        if s[left] in caseL:
+            caseL[s[left]] += 1
         else:
-            letters[str[left]] = 1
+            caseL[s[left]] = 1
+        if s[left].lower() in L:
+            L[s[left].lower()] += 1
+        else:
+            L[s[left].lower()] = 1
         if left < right:
-            if str[right] in letters:
-                letters[str[right]] += 1
+            if s[right] in caseL:
+                caseL[s[right]] += 1
             else:
-                letters[str[right]] = 1
+                caseL[s[right]] = 1
+            if s[right].lower() in L:
+                L[s[right].lower()] += 1
+            else:
+                L[s[right].lower()] = 1         
         left += 1
         right -= 1
+    for c in t:
+        if c in caseL:
+            y += 1
+            caseL[c] -= 1
+        if c.lower() in L:
+            w += 1
+            L[c.lower()] -= 1
+    return y,w
 
 s = input()
 t = input()
-lettersT = {}
-checkLetters(t,lettersT)
-matchCL = 0
-matchL = 0
-for char in s:
-    if char not in lettersT:
-        lettersT[char.swapcase()] -= 1
-        matchL += 1
-    elif lettersT[char] > 0:
-        lettersT[char] -= 1
-        matchCL += 1
-    else:
-        lettersT[char.swapcase()] -= 1
-        matchL += 1
-print(f"{matchCL} {matchL}")
+caseL = {}
+L = {}
+y,w = checkMatches(s,t,caseL,L)
+print(f"{y} {len(s)-y}")
