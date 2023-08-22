e871883da5db7f0c7f9873ecd74e9dea00c55ba0
adrianmartinez-cg <pedro.martinez@ccc.ufcg.edu.br>
2023-08-21 22:54:08 -0300

    lista3: playlist

--- lista3/playlist.py
+++ lista3/playlist.py
@@ -1,15 +1,17 @@
 n = int(input())
 k = list(map(int,input().split()))
 u = {}
+currentLen = 0 
 maxLen = 0
-currentLen = 0
+last = 0
 for i in range(n):
     if k[i] not in u:
         u[k[i]] = i
-        currentLen += 1
     else:
-        currentLen = i - u[k[i]]
+        if u[k[i]] >= last:
+            last = u[k[i]] + 1
         u[k[i]] = i
+    currentLen = i - last + 1
     if currentLen > maxLen:
         maxLen = currentLen
 print(maxLen)
