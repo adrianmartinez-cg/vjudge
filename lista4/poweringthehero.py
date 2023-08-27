48bb5ea113825d48b70dbe04806321d40ef56112
adrianmartinez-cg <pedro.martinez@ccc.ufcg.edu.br>
2023-08-26 23:18:52 -0300

    lista4: powering the hero

--- lista4/poweringthehero.py
+++ lista4/poweringthehero.py
@@ -6,18 +6,13 @@ for _ in range(t):
     n = int(input())
     S = list(map(int,input().split()))
     maxh = []
-    minh = []
     power = 0
     for i in range(len(S)):
-        heapq.heappush(minh,(S[i],i))
-        heapq.heappush(maxh,(-S[i],i))
-    bonus = heapq.heappop(maxh)
-    hero = heapq.heappop(minh)
-    while hero[0] == 0:
-        if hero[1] > bonus[1]:
-            power += abs(bonus[0])
-        bonus = heapq.heappop(maxh)
-        hero = heapq.heappop(minh)
+        if S[i] > 0:
+            heapq.heappush(maxh,-S[i])
+        if S[i] == 0:
+            if len(maxh) > 0:
+                power += abs(heapq.heappop(maxh))
     out.append(power)
 for _ in out:
     print(_)
