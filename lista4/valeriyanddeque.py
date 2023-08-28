85c8b3a39314d5ecdcf14d72e9081003fc9b1265
adrianmartinez-cg <pedro.martinez@ccc.ufcg.edu.br>
2023-08-27 23:05:10 -0300

    lista4: valeriy and deque

--- lista4/valeriyanddeque.py
+++ lista4/valeriyanddeque.py
@@ -1,14 +1,21 @@
 n,q = list(map(int,input().split()))
 A = list(map(int,input().split()))
-Amax = max(A)
-maxPos = A.index(Amax)
+Amax = -1
+maxPos = -1
+for i in range(len(A)):
+    if A[i] > Amax:
+        Amax = A[i]
+        maxPos = i
 queries = []
+queriesUnsorted = []
 d = {}
 maxQuery = 0
 for i in range(q):
     m = int(input())
-    queries.append((m,i+1))
-    d[i+1] = ""
+    query = (m,i+1)
+    queries.append(query)
+    queriesUnsorted.append(query)
+    d[query] = ""
     if m > maxQuery: maxQuery = m
 queries.sort()
 k = 0
@@ -19,10 +26,10 @@ lastR = 0
 
 if q > 0:
     for i in range(min(n,maxQuery)):
-        nextQ = queries[k][0]
+        queryNum = i + 1
         left , right = A[l] , A[r]
         if left == Amax:
-            lastQ = i + 1
+            lastQ = queryNum
             lastR = r
             break
         if left > right:
@@ -30,17 +37,25 @@ if q > 0:
             A[r] = left
         else:
             A.append(left)
-        if i + 1 == nextQ:
-            d[k+1] = f"{left} {right}"
+        nextQ = queries[k]
+        nextQNum = nextQ[0]
+        while queryNum == nextQNum:
+            d[nextQ] = f"{left} {right}"
             k += 1
+            if k < len(queries):
+                nextQ = queries[k]
+                nextQNum = nextQ[0]
+            else:
+                break
         l += 1
         r += 1
     elemsLeft = len(A) - lastQ
     for i in range(k,len(queries)):
-        m = queries[i][0] 
+        query = queries[i]
+        m = query[0] 
         r = lastR + (m - lastQ) % elemsLeft
         right = A[r]
-        d[i+1] = f"{left} {right}"
-    for i in range(len(queries)):
-        query = queries[i][1]
+        d[query] = f"{left} {right}"
+    for i in range(len(queriesUnsorted)):
+        query = queriesUnsorted[i]
         print(d[query])
