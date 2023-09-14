6fbe283f6a57cef39664ca9eb82322f20d6c6ced
adrianmartinez-cg <pedro.martinez@ccc.ufcg.edu.br>
2023-09-13 22:56:23 -0300

    lista6 : just add it [modificacao]

--- lista6/justaddit.py
+++ lista6/justaddit.py
@@ -5,20 +5,23 @@ def fastPowMod(a,b,m):
         if b % 2 == 1:
             c = (c * a) % m
         a = (a * a) % m
-        b >>= 1
+        b //= 2
     return c
 
 p = int(1e7 + 7)
 out = []
-# calcular [n ^ k + 2(n-1)^k + n ^ n + 2(n-1)^n-1] mod p
 while True:
     n,k = list(map(int,input().split()))
     if (n,k) == (0,0): break
     exp1 = fastPowMod(n,k,p)
-    exp2 = (2 * fastPowMod(n-1,k,p))%p
+    exp2 = 2 * fastPowMod(n-1,k,p)
     exp3 = fastPowMod(n,n,p)
-    exp4 = (2* fastPowMod(n-1,n-1,p))%p
-    exp = ((exp1+exp2)%p + (exp3+exp4)%p)%p
+    exp4 = 2* fastPowMod(n-1,n-1,p)
+    exps = [exp1,exp2,exp3,exp4]
+    exp = 0
+    for x in exps:
+        exp += x
+        exp %= p
     out.append(exp)
-for _ in out:
-    print(_)
+for ans in out:
+    print(ans)
