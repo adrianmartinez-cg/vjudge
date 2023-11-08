0671277d7b59e4feb3c2c1bdd7433711c985e1a2
adrianmartinez-cg <pedro.martinez@ccc.ufcg.edu.br>
2023-11-08 13:25:28 -0300

    lista11: projects

--- lista11/projects.cpp
+++ lista11/projects.cpp
@@ -12,22 +12,18 @@ bool cmpSort(const tuple<ll, ll,ll>& a, const tuple<ll,ll,ll>& b){
 ll solve(int i , vector<ll>& dp, vector<tuple<ll,ll,ll>>& P){
     int n = dp.size();
     if(i >= n){
-        cout << "i: " << i << " return 0" << "\n";
         return 0;
     }
     if(dp[i] != -1){
-        cout << "i: " << i << " return dp[i]: " << dp[i] << "\n";
         return dp[i];
     }
     tuple<ll,ll,ll> p = P[i];
     ll end = get<1>(p);
     ll v = get<2>(p);
-    auto it = upper_bound(P.begin(),P.end(),make_tuple(end,end,end));
+    auto it = upper_bound(P.begin(),P.end(),make_tuple(end,end,end),cmpSort);
     int next = it - P.begin();
-    cout << "i: " << i << " next: " << next << "\n";
     ll ans = max(solve(i+1,dp,P), v + solve(next,dp,P));
     dp[i] = ans;
-    cout << "i: " << i << " writing: " << ans << "\n";
     return dp[i];
 }
 
@@ -44,14 +40,8 @@ int main(){
         P[i] = {a,b,p};
     }
     sort(P.begin(),P.end(),cmpSort);
-    solve(0,dp,P);
-    for (int j = 0; j < n; j++)
-    {
-        cout << dp[j] << " ";
-    }
-    cout << "\n";
-    
-    cout << dp[n-1] << "\n";
+    solve(0,dp,P);  
+    cout << dp[0] << "\n";
  
     return 0;
 }
