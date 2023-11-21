4f6ae7b509f66954fdac41bf863c0f3615998f4e
adrianmartinez-cg <pedro.martinez@ccc.ufcg.edu.br>
2023-11-21 10:59:50 -0300

    Header cpp

---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define endl "\n"
+#define rep(i,a,b) for(ll i = (a); i < (b); i++)
+#define sorta(arr) sort((arr).begin(), (arr).end())
+
+int main(){
+    
+}
